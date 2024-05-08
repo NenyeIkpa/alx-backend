f363718bb66b5d7254ed403131c0d8bc4727827e
NenyeIkpa <nenyeikpa@gmail.com>
2024-05-08 12:47:40 +0100

    task 2

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -18,7 +18,7 @@ class FIFOCache(BaseCaching):
         """ Add an item in the cache """
         if key is not None and item is not None:
             self.cache_data[key] = item
-        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+        if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
             last_in_key, _ = self.cache_data.popitem(True)
             print("DISCARD:", last_in_key)
 

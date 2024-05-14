48e1a6a2664d945a0e10223bbca66a401e15e408
NenyeIkpa <nenyeikpa@gmail.com>
2024-05-14 12:45:28 +0000

    fixed syntax errors

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -13,7 +13,7 @@ app = Flask(__name__)
 @app.route('/')
 def index() -> str:
     """ prints text in html format """
-    return render_template('templates/index.html')
+    return render_template('0-index.html')
 
 
 if __name__ == '__main__':

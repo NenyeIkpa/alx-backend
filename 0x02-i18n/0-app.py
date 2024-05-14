e6c063075735bb4f8b14f6b4af80e888e37eaa0f
NenyeIkpa <nenyeikpa@gmail.com>
2024-05-14 12:27:04 +0000

    update

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -14,3 +14,7 @@ app = Flask(__name__)
 def index() -> str:
     """ prints text in html format """
     return render_template('templates/index.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
